839ad9912ddd6975cd9a51d167e53c71e7d59458
Kirill Antonov <antonovkir2@gmail.com>
2023-03-06 14:17:04 +0500

    inc dec funcs

--- src/Counter/Counter.tsx
+++ src/Counter/Counter.tsx
@@ -1,13 +1,27 @@
+import { useState } from 'react'
 import s from './Counter.module.scss'
 
 export const Counter = () => {
+	const [count, setCount] = useState(0)
+
+	const plus = () => {
+		setCount(state => state + 1)
+	}
+	const minus = () => {
+		setCount(state => state - 1)
+	}
+
 	return (
 		<div className='App'>
 			<div>
 				<h2>Счетчик:</h2>
-				<h1>0</h1>
-				<button className={s.minus}>- Минус</button>
-				<button className={s.plus}>Плюс +</button>
+				<h1>{count}</h1>
+				<button className={s.minus} onClick={minus}>
+					- Минус
+				</button>
+				<button className={s.plus} onClick={plus}>
+					Плюс +
+				</button>
 			</div>
 		</div>
 	)
